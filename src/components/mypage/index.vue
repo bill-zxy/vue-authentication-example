13a8bb6d449f526e74bc0e62b70749c7af732c45
bill zxy <bill.zxy@qq.com>
2021-01-26 08:52:41 +0800

    --- Auto-Git Commit ---

--- src/components/mypage/index.vue
+++ src/components/mypage/index.vue
@@ -2,15 +2,7 @@
 
 <template>
   <div class = "mypage">
-    <div v-if="loading" class="loading">
-      Loading...
-    </div>
-    <div v-if="error" class="error">
-      {{error}}
-    </div>
-    <div v-if="post" class="post">    
-     <p v-html="content">{{content}}</p>
-    </div> 
+     <p v-html="content"></p>
   </div>
 </template>
 <style>
@@ -22,41 +14,17 @@
 </style>
 
 <script>
-
+import mapState from "vuex";
 export default {
   name: "mypage",
   data() {
         return {
-          loading:false,
-          post:"Hello World!",
-          error:null
+          post:"Hello World!"
         };
   },
-  created() {
-     console.log("My page is created!");
-     this.fetchData();
-  },
-  watch:{
-    '$route':'fetchData'
-  },
-  computed:{
-    content() {
-      return this.$store.state.data;
-    },
-  },
-  methods:{
-     fetchData () {
-       this.error = this.post = null;
-       this.loading = true;
-       const fetchedID = this.$route.params.id;
-       console.log(fetchedID);
-       //get the account from store
-       this.post = this.$store.getPageData; 
-       console.log("the value of post in page module of store is:");
-       console.log(this.post);
-       //set state status
-       this.loading = false;
-     },
-  },
-};
+ 
+  computed:mapState ({
+    content:state => this.$store.state.data
+    })
+ };
 </script>
